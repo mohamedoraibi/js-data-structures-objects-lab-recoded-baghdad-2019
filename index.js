e8a4b50e34bf18d7c5464d591d5684029bdb073a
Mohammed Al-Oraibi <mohamed.b.oraibi@gmail.com>
2019-09-18 06:44:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const driver = { name: '11 Broadway' };
 
 function updateDriverWithKeyAndValue(driver, key, value){
   const newObj = Object.assign({}, driver);
-  newObj.name="Sam";
+  newObj['name']="Sam";
   return newObj;
 }
 
